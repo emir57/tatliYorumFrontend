28618a8b257003cafc4b0efdc8e3c8ce285c2219
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-27 16:57:16 +0300

    showLoading

--- src/app/services/loading.service.ts
+++ src/app/services/loading.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
 
-  constructor() { }
+  constructor(
+    private loadingController: LoadingController
+  ) { }
+
+  async showLoading(message: string) {
+    const loading = await this.loadingController.create({
+      message: message,
+      duration: 4000
+    })
+    return await loading.present();
+  }
 }
