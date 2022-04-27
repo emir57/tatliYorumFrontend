73e22ae10d797c7221a280c6fc7e954b65fb825c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-27 17:16:49 +0300

    authService register and isLogin check

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { User } from 'src/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  isLogin: boolean = false;
+  constructor(
+    @Inject("baseUrl") private baseUrl: string,
+    private http: HttpClient
+  ) { }
+
+  register(user: User) {
+    let url = `${this.baseUrl}/api/kayit`;
+    return this.http.post<string>(url, user);
+  }
 }
