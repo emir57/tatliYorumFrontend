6d51c7087b79fa673c0bb54257faef6af4c4eae4
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-27 17:44:55 +0300

    create message service

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor(
+    private toastController: ToastController
+  ) { }
+
+  async showMessage() {
+    const toast = await this.toastController.create({
+      message: 'Your settings have been saved.',
+      duration: 2000
+    });
+    toast.present();
+  }
+}
