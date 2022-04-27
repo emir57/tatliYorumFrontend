dbe5edf859536caad78a57cbce41ec5cf09b9dad
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-27 17:56:56 +0300

    create enum KeyType and setName

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@capacitor/storage';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+  constructor(
+  ) { }
+
+  async setName(key: KeyType, value: string) {
+    await Storage.set({
+      key: key,
+      value: value
+    })
+  }
+}
+export enum KeyType {
+  User = "tatliYorum_user"
 }
