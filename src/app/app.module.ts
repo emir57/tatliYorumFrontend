e0ba2323f911e8b3b3486c4bb68e227473ca68cf
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-27 15:58:12 +0300

    import color picker module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from "@angular/common/http";
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { HttpClientModule } from "@angular/common/http";
     HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    ColorPickerModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
