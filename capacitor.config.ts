8e2d0f6235db570412d50c736d845f9734d6fe0a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-27 15:31:17 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tatliYorumFrontend',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
