3171ad8b9312a685151d4977c41705d8d448afa6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-27 15:37:51 +0300

    Add Settings page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'post-add',
     loadChildren: () => import('./post-add/post-add.module').then( m => m.PostAddPageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
   }
 ];
 
