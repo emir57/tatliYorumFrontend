e8757221f7748756824721cb6f137eadeed6a680
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-27 16:02:17 +0300

    post model

---
+++ src/models/post.ts
@@ -0,0 +1,10 @@
+export interface Post {
+  id: number;
+  content: string;
+  categoryId: number;
+  userId: number;
+  isAnimation: boolean;
+  backgroundColor: string;
+  textColor: string;
+  secretUser: boolean;
+}
