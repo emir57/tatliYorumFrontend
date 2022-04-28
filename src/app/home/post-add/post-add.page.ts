57d7fe8ab9585c9f3890c83983a4f793fb2d07fb
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-28 16:21:24 +0300

    add function

--- src/app/home/post-add/post-add.page.ts
+++ src/app/home/post-add/post-add.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { LoadingService } from 'src/app/services/loading.service';
 import { MessageService } from 'src/app/services/message.service';
+import { Post } from 'src/models/post';
 
 @Component({
   selector: 'app-post-add',
@@ -21,6 +22,14 @@ export class PostAddPage implements OnInit {
   ) { }
 
   ngOnInit() {
+  }
+
+
+  add() {
+    if (this.saveForm.valid) {
+      let post: Post = Object.assign({}, this.saveForm.value);
+      console.log(post)
+    }
   }
 
 }
