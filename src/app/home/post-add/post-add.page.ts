fc383694a84a40d882938cc99659b7217dd39d45
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-28 16:20:28 +0300

    post add page import services

--- src/app/home/post-add/post-add.page.ts
+++ src/app/home/post-add/post-add.page.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { LoadingService } from 'src/app/services/loading.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-post-add',
@@ -7,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostAddPage implements OnInit {
 
-  constructor() { }
+  saveForm: FormGroup;
+  isLoad: boolean = true;
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private messageService: MessageService,
+    private loadingService: LoadingService
+  ) { }
 
   ngOnInit() {
   }
