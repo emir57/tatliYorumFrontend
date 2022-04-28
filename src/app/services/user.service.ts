64995c2135985fd8e1c7d57aa531698a77b8a0bc
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-28 16:35:26 +0300

    getUser

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/models/user';
+import { KeyType, StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private storageService: StorageService
+  ) { }
+
+  async getUser() {
+    let user: User = JSON.parse(await this.storageService.checkName(KeyType.User))
+    return user;
+  }
 }
