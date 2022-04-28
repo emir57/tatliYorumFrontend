05a5361c591c89c86a282edf118094ca66be6ce8
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-28 16:21:43 +0300

    import ReactiveFormsModule

--- src/app/home/post-add/post-add.module.ts
+++ src/app/home/post-add/post-add.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { PostAddPage } from './post-add.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    PostAddPageRoutingModule
+    PostAddPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [PostAddPage]
 })
-export class PostAddPageModule {}
+export class PostAddPageModule { }
