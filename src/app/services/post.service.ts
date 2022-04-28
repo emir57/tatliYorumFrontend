12089ca14f5f665ff9e1bb35be1349ab14e2d92e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-28 15:45:30 +0300

    postadd url

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
+import { Post } from "../../models/post";
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,9 @@ export class PostService {
   constructor(
     @Inject("baseUrl") private baseUrl: string
   ) { }
+
+
+  add(post: Post) {
+    let url = `${this.baseUrl}api/postadd`;
+  }
 }
