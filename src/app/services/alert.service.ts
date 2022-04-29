084ddba48ed32d868c9b55791e1c7f9c1f17e7d9
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 23:06:13 +0300

    Create AlertService

---
+++ src/app/services/alert.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor(
+    private alertController: AlertController
+  ) { }
+}
