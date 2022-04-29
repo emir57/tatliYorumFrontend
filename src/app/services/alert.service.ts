4511a841066fff1e0f7009adfcbaa575c2d45482
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 23:06:42 +0300

    showAlertConfirm func

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -9,4 +9,32 @@ export class AlertService {
   constructor(
     private alertController: AlertController
   ) { }
+
+
+  async showAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
