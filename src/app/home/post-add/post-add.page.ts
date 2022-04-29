3fc6629aefead7e6e112fbe0d90c988a8dcae3da
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-29 22:30:00 +0300

    post add

--- src/app/home/post-add/post-add.page.ts
+++ src/app/home/post-add/post-add.page.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { LoadingService } from 'src/app/services/loading.service';
 import { MessageService } from 'src/app/services/message.service';
+import { PostService } from 'src/app/services/post.service';
 import { UserService } from 'src/app/services/user.service';
 import { Category } from 'src/models/category';
 import { Post } from 'src/models/post';
@@ -29,7 +31,9 @@ export class PostAddPage implements OnInit {
     private userService: UserService,
     private http: HttpClient,
     private messageService: MessageService,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private postService: PostService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -59,10 +63,17 @@ export class PostAddPage implements OnInit {
   }
 
   add() {
-    // if (this.saveForm.valid) {
-    let post: Post = Object.assign({}, this.saveForm.value);
-    console.log(post)
-    // }
+    if (this.saveForm.valid) {
+      let post: Post = Object.assign({}, this.saveForm.value);
+      this.postService.add(post).subscribe(response => {
+        if (response.success) {
+          this.messageService.showMessage(response.message, {});
+          this.router.navigateByUrl("/home/posts")
+        } else {
+          this.messageService.showMessage(response.message, {});
+        }
+      })
+    }
   }
 
   getColorStyles() {
