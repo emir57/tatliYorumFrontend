cbd731f7f52208b79a48f813c3bd9d0db9fecca0
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-02 02:37:13 +0300

    inject http and baseUrl

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(
+    @Inject("baseUrl") private baseUrl: string,
+    private http: HttpClient
+  ) { }
 }
