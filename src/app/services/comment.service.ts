a9fee36ef0e1641e91b710af13f77c0ed06a8680
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-02 02:39:48 +0300

    getAllByPostId

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -17,4 +17,9 @@ export class CommentService {
     let url = `${this.baseUrl}/api/comments`;
     return this.http.get<ResponseDataListModel<PostComment>>(url);
   }
+
+  getAllByPostId(postId: number) {
+    let url = `${this.baseUrl}/api/postcomments/${postId}`;
+    return this.http.get<ResponseDataListModel<PostComment>>(url);
+  }
 }
