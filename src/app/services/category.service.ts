f5122bc08f8e163d8c453171075d9d599c1c9245
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-01 22:55:41 +0300

    category service

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { Category } from 'src/models/category';
+import { ResponseDataListModel } from 'src/models/responseDataModel';
+import { ResponseModel } from 'src/models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +13,22 @@ export class CategoryService {
     @Inject("baseUrl") private baseUrl: string,
     private http: HttpClient
   ) { }
+
+
+  getAll() {
+    let url = `${this.baseUrl}/api/categories`;
+    return this.http.get<ResponseDataListModel<Category>>(url);
+  }
+  add(category: Category) {
+    let url = `${this.baseUrl}/api/categoryadd`;
+    return this.http.post<ResponseModel>(url, category);
+  }
+  update(category: Category) {
+    let url = `${this.baseUrl}/api/categoryedit`;
+    return this.http.post<ResponseModel>(url, category);
+  }
+  delete(categoryId: number) {
+    let url = `${this.baseUrl}/api/categorydelete/${categoryId}`;
+    return this.http.delete<ResponseModel>(url);
+  }
 }
