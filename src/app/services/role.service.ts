4b177d50a1eb43876786e1739b982d791865fa28
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-04 16:03:54 +0300

    role service checkIsAdmin()

--- src/app/services/role.service.ts
+++ src/app/services/role.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/models/user';
+import { LoadingService } from './loading.service';
+import { KeyType, StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleService {
 
-  constructor() { }
+  isAdmin: boolean = false;
+  constructor(
+    private loadingService: LoadingService,
+    private storageService: StorageService
+  ) {
+    this.checkIsAdmin();
+  }
+
+  async checkIsAdmin() {
+    this.loadingService.showLoading("İşlemler yapılıyor lütfen bekleyiniz.");
+    let user: User = JSON.parse(await this.storageService.checkName(KeyType.User));
+    this.isAdmin = user.isAdmin;
+    this.loadingService.closeLoading();
+  }
 }
