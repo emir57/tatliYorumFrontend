5007a64a284c48fecd36ae7e92f4e09fe2a09d03
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-04 15:56:44 +0300

    create admin categories page

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: AdminPage
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./categories/categories.module').then( m => m.CategoriesPageModule)
   }
 ];
 
