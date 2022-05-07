5067a2cba93b6c9eeccb1bf3e48418d23cc93fb1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-07 15:27:34 +0300

    getComments

--- src/app/admin/comments/comments.page.ts
+++ src/app/admin/comments/comments.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AlertService } from 'src/app/services/alert.service';
+import { CommentService } from 'src/app/services/comment.service';
+import { MessageService } from 'src/app/services/message.service';
+import { UserService } from 'src/app/services/user.service';
 import { PostComment } from 'src/models/postComment';
 
 @Component({
@@ -9,9 +14,29 @@ import { PostComment } from 'src/models/postComment';
 export class CommentsPage implements OnInit {
 
   comments: PostComment[];
-  constructor() { }
+  constructor(
+    private alertService: AlertService,
+    private commentService: CommentService,
+    private messageService: MessageService,
+    private modalController: ModalController,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.getComments();
+  }
+
+  getComments() {
+    this.commentService.getAll().subscribe(response => {
+      if (response.success) {
+        this.comments = response.data;
+        this.comments.forEach(comment => {
+          this.userService.getUserById(comment.userId).subscribe(r=>{
+            comment.user = r.data;
+          })
+        })
+      }
+    })
   }
 
 }
