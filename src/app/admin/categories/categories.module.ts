9a917456a06f3071016e45c065d01e34f9829827
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-08 14:05:01 +0300

    categories module import ReactiveFormsModule

--- src/app/admin/categories/categories.module.ts
+++ src/app/admin/categories/categories.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { CategoriesPage } from './categories.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    CategoriesPageRoutingModule
+    CategoriesPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [CategoriesPage]
 })
-export class CategoriesPageModule {}
+export class CategoriesPageModule { }
