7a73b28c0f0ff61b50d70459ad83a63dfc541c4f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-08 14:05:55 +0300

    category edit close function

--- src/app/admin/category-edit/category-edit.page.ts
+++ src/app/admin/category-edit/category-edit.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-category-edit',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryEditPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalController: ModalController
+  ) { }
 
   ngOnInit() {
+  }
+
+  async close() {
+    await this.modalController.dismiss();
   }
 
 }
