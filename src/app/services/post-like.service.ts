b7a7db44c678b453784471386f51d5249c403e78
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-08 14:46:11 +0300

    post like service getAll and delete

--- src/app/services/post-like.service.ts
+++ src/app/services/post-like.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { PostLike } from 'src/models/postLike';
+import { ResponseDataListModel } from 'src/models/responseDataModel';
+import { ResponseModel } from 'src/models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostLikeService {
 
-  constructor() { }
+  constructor(
+    @Inject("baseUrl") private baseUrl: string,
+    private http: HttpClient
+  ) { }
+
+  getAll() {
+    let url = `${this.baseUrl}/api/postlikes`;
+    return this.http.get<ResponseDataListModel<PostLike>>(url);
+  }
+  delete(id: number) {
+    let url = `${this.baseUrl}/api/deletepostlike/${id}`;
+    return this.http.delete<ResponseModel>(url);
+  }
 }
