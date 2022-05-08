03249fdfab40a13009316894257c18fc5f260bea
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-08 14:28:24 +0300

    add function

--- src/app/admin/category-add/category-add.page.ts
+++ src/app/admin/category-add/category-add.page.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
 import { MessageService } from 'src/app/services/message.service';
+import { Category } from 'src/models/category';
 
 @Component({
   selector: 'app-category-add',
@@ -30,7 +31,19 @@ export class CategoryAddPage implements OnInit {
   }
 
   add() {
-
+    if (this.saveForm.valid) {
+      let category: Category = this.saveForm.value;
+      this.categoryService.add(category).subscribe(response => {
+        if (response.success) {
+          this.messageService.showMessage(response.message, {});
+          setTimeout(() => {
+            this.router.navigateByUrl("/admin/home");
+          }, 100);
+        } else {
+          this.messageService.showMessage(response.message, {});
+        }
+      })
+    }
   }
 
 }
