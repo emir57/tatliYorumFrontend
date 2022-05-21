27328ecfbb77fcab036e42554a0a97d0c14cefd1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-21 13:36:44 +0300

    userService getAll

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from '@angular/core';
 import { ResponseDataModel } from 'src/models/responseDataModel';
 import { ResponseModel } from 'src/models/responseModel';
 import { User } from 'src/models/user';
+import { LoadingService } from './loading.service';
 import { KeyType, StorageService } from './storage.service';
 
 @Injectable({
@@ -13,7 +14,8 @@ export class UserService {
   constructor(
     @Inject("baseUrl") private baseUrl: string,
     private storageService: StorageService,
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private loadService: LoadingService
   ) { }
 
   async getUser() {
@@ -24,5 +26,18 @@ export class UserService {
   getUserById(userId: number) {
     let url = `${this.baseUrl}/api/users/${userId}`;
     return this.httpClient.get<ResponseDataModel<User>>(url);
+  }
+
+  async getAll(successCallBack?: (response: ResponseDataModel<User[]>) => void, errorCallBack?: () => void) {
+    await this.loadService.showLoading("");
+    let url = `${this.baseUrl}/api/users`;
+    this.httpClient.get<ResponseDataModel<User[]>>(url).subscribe(async response => {
+      if (response.success) {
+        successCallBack(response);
+      } else {
+        errorCallBack();
+      }
+      await this.loadService.closeLoading();
+    })
   }
 }
