cb80c3b599ade81b5e1791e6ccd871317ab4d32f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-21 13:39:04 +0300

    get users in users page

--- src/app/admin/users/users.page.ts
+++ src/app/admin/users/users.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/models/user';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersPage implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.getAll(
+      (response) => {
+        this.users = response.data;
+      },
+      () => { }
+    );
   }
 
 }
