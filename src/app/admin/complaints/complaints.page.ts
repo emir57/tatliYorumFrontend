f4b3519002583910adbf8fd2135d2f7e8a97b840
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-25 13:57:55 +0300

    complaint delete in array and text-danger class

--- src/app/admin/complaints/complaints.page.ts
+++ src/app/admin/complaints/complaints.page.ts
@@ -6,6 +6,7 @@ import { AlertService } from 'src/app/services/alert.service';
 import { ComplaintService } from 'src/app/services/complaint.service';
 import { MessagePosition, MessageService } from 'src/app/services/message.service';
 import { Complaint } from 'src/models/complaint';
+declare var $: any;
 
 @Component({
   selector: 'app-complaints',
@@ -45,17 +46,25 @@ export class ComplaintsPage implements OnInit {
   }
 
   deleteComplaint(complaint: Complaint) {
+    const complaintItem = $("#complaint" + complaint.id);
+    complaintItem.addClass("text-danger");
     this.alertService.showAlertConfirm("Silme işlemi", "Bu şikayeti silmek istediğinizden emin misiniz?",
-      () => { },
+      () => { complaintItem.removeClass("text-danger"); },
       () => {
         this.complaintService.delete(complaint.id).subscribe(response => {
           if (response.success) {
+            this.deleteComplaintInArray(complaint);
             this.messageService.showMessage(response.message, { position: MessagePosition.Top })
           } else {
+            complaintItem.removeClass("text-danger");
             this.messageService.showMessage(response.message, { position: MessagePosition.Top })
           }
         })
       })
+  }
+  deleteComplaintInArray(complaint: Complaint) {
+    let i = this.complaints.findIndex(c => c.id === complaint.id);
+    this.complaints.splice(i, 1);
   }
 
 }
