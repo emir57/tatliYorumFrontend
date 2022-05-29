2064bc802c782591de2c693ea16b6b5c51487358
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-29 17:08:41 +0300

    import ReactiveFormsModule in edit user page

--- src/app/admin/edit-user/edit-user.module.ts
+++ src/app/admin/edit-user/edit-user.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,9 @@ import { EditUserPage } from './edit-user.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    EditUserPageRoutingModule
+    EditUserPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [EditUserPage]
 })
-export class EditUserPageModule {}
+export class EditUserPageModule { }
